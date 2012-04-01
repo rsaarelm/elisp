03e27fc8da8a0357319981a97e0f36fde223327e
Risto Saarelma <risto.saarelma@iki.fi>
2012-04-01 19:49:20 +0300

    Reftex for org

--- orgmode.el
+++ orgmode.el
@@ -205,3 +205,28 @@
 
 ; Get \Perp symbol work for stochastic text.
 (setq org-export-latex-append-header "\\newcommand{\\Perp}{\\perp \\! \\! \\! \\perp}")
+
+
+(defun org-mode-reftex-setup ()
+  (load-library "reftex")
+  (and (buffer-file-name) (file-exists-p (buffer-file-name))
+       (progn
+	 ;enable auto-revert-mode to update reftex when bibtex file changes on disk
+	 (global-auto-revert-mode t)
+	 (reftex-parse-all)
+	 ;add a custom reftex cite format to insert links
+	 (reftex-set-cite-format
+	  '((?b . "[[bib:%l][%l-bib]]")
+	    (?n . "[[notes:%l][%l-notes]]")
+	    (?p . "[[papers:%l][%l-paper]]")
+	    (?t . "%t")
+	    (?h . "** %t\n:PROPERTIES:\n:Custom_ID: %l\n:END:\n[[papers:%l][%l-paper]]")))))
+  (define-key org-mode-map (kbd "C-c )") 'reftex-citation)
+  (define-key org-mode-map (kbd "C-c (") 'org-mode-reftex-search))
+
+(defun org-mode-reftex-search ()
+  ;;jump to the notes for the paper pointed to at from reftex search
+  (interactive)
+  (org-open-link-from-string (format "[[notes::%s]]" (reftex-citation t))))
+
+(add-hook 'org-mode-hook 'org-mode-reftex-setup)
